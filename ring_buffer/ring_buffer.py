cdb2c741c0e21c86953041a1634657695b6b3f53
bodhibrdr <julie.gumerman@gmail.com>
2020-02-28 10:18:35 -0700

    overwrite doesn't work

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -16,13 +16,33 @@ class RingBuffer:
         current_head = self.storage.head
 
         #if there's nothing in the RingBuffer, 
-        if self.current < self.capacity:
+        if current_head == None:
             self.storage.add_to_head(item)
             self.current += 1
             return
-        #if there is stuff...
+        #if there is stuff but we are not at capacity (filling buffer for the first time)
+        elif self.storage.__len__() < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current += 1
+        #storage is full
         else:
-            pass
+            #increment self.current by one
+            self.current += 1
+            if self.current == self.capacity:
+                self.current == 0
+            if self.current == 0:
+                self.storage.head.value == item.value
+            else:
+                #won't work: can't subscript a doubly linked list
+                #self.storage[self.current] == item.value
+                count = 0
+                current = self.storage.head
+                while current != None:
+                    if count == self.current:
+                        self.current.value == item.value
+                    current = current.next
+
+
 
 
 
@@ -31,8 +51,10 @@ class RingBuffer:
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        dll_item = self.storage.head
+        while dll_item != None:
+            list_buffer_contents.append(dll_item.value)
+            dll_item = dll_item.next
 
         return list_buffer_contents
 
