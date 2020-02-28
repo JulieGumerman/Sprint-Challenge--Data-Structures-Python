f289de9ad067ad5fa5798444bb5d9960ac62725b
bodhibrdr <julie.gumerman@gmail.com>
2020-02-28 09:16:00 -0700

    first pass at names

--- names/names.py
+++ names/names.py
@@ -1,7 +1,10 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
+##Before refactoring, this code was polynomial (O(n^2)) because of its nested for-loop
+##Now, it is operating at O(n), or linear.
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -10,13 +13,25 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
+BST = BinarySearchTree("names")
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    BST.insert(name_1)
+
+for name_2 in names_2:
+    if BST.contains(name_2):
+        duplicates.append(name_2)
+# Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
